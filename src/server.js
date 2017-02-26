cb578a895c4d9f0d1617c39576a2c9f007815de4
boeyjw <khcy5bjw@nottingham.edu.my>
2017-02-26 11:27:25 +0800

    updated gbif and ncbi schema

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ console.log("App listening on port 3000");
 var Schema = mongoose.Schema({
 	_id : mongoose.Schema.Types.ObjectId,
 	taxonID : Number,
-	datasetID : mongoose.Schema.Types.Mixed,
+	datasetID : String,
 	parentNameUsageID : Number,
 	acceptNameUsageID : Number,
 	originalNameUsageID : Number,
@@ -44,9 +44,37 @@ var Schema = mongoose.Schema({
 	family : String,
 	genus  : String,
 	taxonRemarks : String,
-	distribution : [],
-	multimedia   : [mongoose.Schema.Types.Mixed],
-	references   : [],
+	distribution : [{
+		threatStatus : String,
+		establishmentMeans : String,
+		lifeStage : String,
+		source : String,
+		country : String,
+		occuranceStatus : String,
+		countryCode : String,
+		locationID : String,
+		locality : String,
+		locationRemarks : String
+	}],
+	multimedia   : [{
+		license : String,
+		rightsHolder : String,
+		creator : String,
+		references : String,
+		contributor : String,
+		source : String,
+		identifier : String,
+		created : String,
+		title : String,
+		publisher : String,
+		description : String
+	}],
+	references   : [{
+		bibliographicCitation : String,
+		references : String,
+		source : String,
+		identifier : String
+	}],
 	vernacularname : [{
 		sex : String,
 		lifeStage : String,
@@ -60,7 +88,46 @@ var Schema = mongoose.Schema({
 
 });
 
+var nSchema = mongoose.Schema({
+	_id : mongoose.Schema.Types.ObjectId,
+	tax_id : Number,
+	parent_tax_id : Number,
+	rank : String,
+	embl_code : String,
+	inherited_div_flag : Number,
+	inherited_GC_flag : Number,
+	mitochondrial_genetic_code_id : Number,
+	inherited_MGC_flag : Number,
+	GenBank_hidden_flag : Number,
+	hidden_subtree_root_flag : Number,
+	comments : String,
+	names : [{
+		name_txt : String,
+		unique_name : String,
+		name_class : String
+	}],
+	division : [{
+		cde : String,
+		name : String,
+		comments : String
+	}],
+	gencode : [{
+		abbreviation : String,
+		name : String,
+		cde : String,
+		starts : String
+	}],
+	citations : [{
+		cit_key : String,
+		pubmed_id : Number,
+		medline_id : Number,
+		url : String,
+		text : String
+	}]
+});
+
 var GBIF = mongoose.model('GBIF', Schema);
+var NCBI = mongoose.model('NCBI', nSchema);
 
 
 app.get('/GBIF', function (req, res) {
@@ -85,6 +152,6 @@ app.get('/GBIF', function(req,res){
 });
 
 app.get('*', function(req, res){
-	res.sendFile('./public/testing.html', { root: __dirname });
+	res.sendFile('./testing.html', { root: __dirname });
 });
 
