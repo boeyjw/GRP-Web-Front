e93a45fcc291eb301becdd779ee643510ae57d30
Unknown <hazwiesyazwan@gmail.com>
2017-04-02 23:38:23 +0800

    routing from homepage to 2nd page done

--- src/public/theMainScript.js
+++ src/public/theMainScript.js
@@ -10,17 +10,46 @@ replantMain.config(function($routeProvider){
 	.when('/result', {
 		templateUrl: 'queryResults_Page(boxes).html'
 	})
+	.when('/view', {
+		templateUrl: 'replant_ViewPage.html'
+	})
 	.otherwise({
 		redirectTo: '/home'
 	});
 });
 
+//new 'service' created to store searched keyword
+//searched keyword will be stored in 'savedKeyword.list'
+
+
+replantMain.factory('searchService', function(){
+	var savedKeyword = {} //new empty object
+	
+	savedKeyword.list = [];
+
+	//function add
+	searchService.add = function(keyword){
+		searchService.list.push({text: keyword});
+	};
+
+	return searchService; //return object
+});
+
+replantMain.controller("searchController", function(searchService){
+	var self - this;
+
+	self.newKeyword = '';
+
+	self.addKeyword = function(keyword){
+		searchService.add(keyword);
+		self.newKeyword = '';
+	};
+
+});
 
 //controller for second page (queryResults_Page(boxes).html)
-replantMain.controller("resultsController", function($scope){
-	/* to be filled with... erm, anything */
+replantMain.controller("resultsController", function(searchService){
+	var self = this;
 
-	$scope.records = [
-		//to be filled
-	]
+	self.searchService = searchService.list;
 });
