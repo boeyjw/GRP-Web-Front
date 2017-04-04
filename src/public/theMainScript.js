8b5545304fb72a8279116acf87a0dfbe61b1880d
Boey <khcy5bjw@nottingham.edu.my>
2017-04-04 11:20:12 +0800

    Minor fix var self - this to var self = this

--- src/public/theMainScript.js
+++ src/public/theMainScript.js
@@ -2,54 +2,54 @@ var replantMain = angular.module("replantMain", ['ngRoute']);
 
 //routing
 
-replantMain.config(function($routeProvider){
-	$routeProvider
-	.when('/home', {
-		templateUrl: 'replant_HomePage.html'
-	})
-	.when('/result', {
-		templateUrl: 'queryResults_Page(boxes).html'
-	})
-	.when('/view', {
-		templateUrl: 'replant_ViewPage.html'
-	})
-	.otherwise({
-		redirectTo: '/home'
-	});
+replantMain.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'replant_HomePage.html'
+        })
+        .when('/result', {
+            templateUrl: 'queryResults_Page(boxes).html'
+        })
+        .when('/view', {
+            templateUrl: 'replant_ViewPage.html'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
 });
 
 //new 'service' created to store searched keyword
 //searched keyword will be stored in 'savedKeyword.list'
 
 
-replantMain.factory('searchService', function(){
-	var savedKeyword = {} //new empty object
-	
-	savedKeyword.list = [];
+replantMain.factory('searchService', function() {
+    var savedKeyword = {} //new empty object
 
-	//function add
-	searchService.add = function(keyword){
-		searchService.list.push({text: keyword});
-	};
+    savedKeyword.list = [];
 
-	return searchService; //return object
+    //function add
+    searchService.add = function(keyword) {
+        searchService.list.push({ text: keyword });
+    };
+
+    return searchService; //return object
 });
 
-replantMain.controller("searchController", function(searchService){
-	var self - this;
+replantMain.controller("searchController", function(searchService) {
+    var self = this;
 
-	self.newKeyword = '';
+    self.newKeyword = '';
 
-	self.addKeyword = function(keyword){
-		searchService.add(keyword);
-		self.newKeyword = '';
-	};
+    self.addKeyword = function(keyword) {
+        searchService.add(keyword);
+        self.newKeyword = '';
+    };
 
 });
 
 //controller for second page (queryResults_Page(boxes).html)
-replantMain.controller("resultsController", function(searchService){
-	var self = this;
+replantMain.controller("resultsController", function(searchService) {
+    var self = this;
 
-	self.searchService = searchService.list;
+    self.searchService = searchService.list;
 });
