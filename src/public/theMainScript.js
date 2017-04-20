2edaaaf8608a50aebafc6806847e39cd2bdab7e3
Unknown <hazwiesyazwan@gmail.com>
2017-04-21 03:17:15 +0800

    3rd page update, without nested ng-repeat

--- src/public/theMainScript.js
+++ src/public/theMainScript.js
@@ -1,104 +1,104 @@
-var replantMain = angular.module("replantMain", ['ngRoute']);
-
-//routing
-
-replantMain.config(function($routeProvider) {
-    $routeProvider
-        .when('/home', {
-            templateUrl: 'replant_HomePage.html'
-        })
-        .when('/result', {
-            templateUrl: 'queryResults_Page(boxes).html'
-        })
-        .when('/view', {
-            templateUrl: 'replant_ViewPage.html'
-        })
-        .otherwise({
-            redirectTo: '/home'
-        });
-});
-
-//new 'service' created to store searched keyword
-//searched keyword will be stored in 'savedKeyword.list'
-
-
-/*replantMain.factory('searchService', function() {
-    var searchService = {} //new empty object
-
-    searchService.list = [];
-
-    //function add
-    searchService.add = function(keyword) {
-        searchService.list.push({ text: keyword });
-    };
-
-    return searchService; //return object
-});*/
-
-var keyword;
-var keyword2;
-
-replantMain.controller("searchController", function($scope, $route) {
-    var self = this;
-
-    //self.newKeyword = '';
-
-    self.addKeyword = function() {
-        //searchService.add(keyword);
-        //self.newKeyword = '';
-        keyword = $scope.newKeyword;
-        $route.reload("#/result");
-    };
-
-});
-
-
-
-//controller for second page (queryResults_Page(boxes).html)
-replantMain.controller("resultsController", function($http, $scope, $route, $location) {
-    var self = this;
-
-    //self.searchService = searchService.list;
-
-    $http.get('/find/' + keyword).then(function(res) {
-        console.log(res.data);
-        $scope.resultset = res.data;
-    })
-
-    self.addKeyword2 = function($index) {
-        //searchService.add(keyword);
-        //self.newKeyword = '';
-        console.log($scope.resultset[$index]._id);
-        keyword2 = $scope.resultset[$index]._id;
-        $location.path("#/view");
-    };
-
-
-});
-
-//================================================================================================================================================================
-/*replantMain.controller("search2Controller", function($scope, $route) {
-    var self = this;
-
-    //self.newKeyword = '';
-
-    self.addKeyword = function() {
-        //searchService.add(keyword);
-        //self.newKeyword = '';
-        keyword2 = $scope.thePlant;
-        $route.reload("#/view");
-    };
-
-});*/
-
-replantMain.controller("displayAll", function($http, $scope, $location) {
-    console.log(keyword2);
-    $http.get('/result/' + keyword2).then(function(res, err) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(res.data);
-            $scope.thePlant = res.data;
-        }
-    })
+var replantMain = angular.module("replantMain", ['ngRoute']);
+
+//routing
+
+replantMain.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'replant_HomePage.html'
+        })
+        .when('/result', {
+            templateUrl: 'queryResults_Page(boxes).html'
+        })
+        .when('/view', {
+            templateUrl: 'replant_ViewPage.html'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
+
+//new 'service' created to store searched keyword
+//searched keyword will be stored in 'savedKeyword.list'
+
+
+/*replantMain.factory('searchService', function() {
+    var searchService = {} //new empty object
+
+    searchService.list = [];
+
+    //function add
+    searchService.add = function(keyword) {
+        searchService.list.push({ text: keyword });
+    };
+
+    return searchService; //return object
+});*/
+
+var keyword;
+var keyword2;
+
+replantMain.controller("searchController", function($scope, $route) {
+    var self = this;
+
+    //self.newKeyword = '';
+
+    self.addKeyword = function() {
+        //searchService.add(keyword);
+        //self.newKeyword = '';
+        keyword = $scope.newKeyword;
+        $route.reload("#/result");
+    };
+
+});
+
+
+
+//controller for second page (queryResults_Page(boxes).html)
+replantMain.controller("resultsController", function($http, $scope, $route, $location) {
+    var self = this;
+
+    //self.searchService = searchService.list;
+
+    $http.get('/find/' + keyword).then(function(res) {
+        console.log(res.data);
+        $scope.resultset = res.data;
+    })
+
+    self.addKeyword2 = function($index) {
+        //searchService.add(keyword);
+        //self.newKeyword = '';
+        console.log($scope.resultset[$index]._id);
+        keyword2 = $scope.resultset[$index]._id;
+        $location.path("#/view");
+    };
+
+
+});
+
+//================================================================================================================================================================
+/*replantMain.controller("search2Controller", function($scope, $route) {
+    var self = this;
+
+    //self.newKeyword = '';
+
+    self.addKeyword = function() {
+        //searchService.add(keyword);
+        //self.newKeyword = '';
+        keyword2 = $scope.thePlant;
+        $route.reload("#/view");
+    };
+
+});*/
+
+replantMain.controller("displayAll", function($http, $scope, $location) {
+    console.log(keyword2);
+    $http.get('/result/' + keyword2).then(function(res, err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(res.data);
+            $scope.thePlant = res.data;
+        }
+    })
 });
